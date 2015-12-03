8b9e9ebdd0a79303fe675352b2c329c5d9d3e4c9
malaue4 <malaue4@gmail.com>
2015-12-03 16:45:53 +0100

    Boop

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -3,8 +3,9 @@ using System.Collections;
 
 public class Enemy : MonoBehaviour {
 
+    public GameObject explosion;
 
-    float health;
+    float health=4;
 	public enum enemyTypes {
         Weak, Middle, Strong,
     }
@@ -13,24 +14,29 @@ public class Enemy : MonoBehaviour {
 	void Start () {
         if (type == enemyTypes.Weak)
         {
-            health = 2;
+            health *= 0.5f;
         }
-	}
+        if (type == enemyTypes.Middle) {
+            health *= 1.0f;
+        }
+        if (type == enemyTypes.Strong) {
+            health *= 2.0f;
+        }
+    }
 	
 	// Update is called once per frame
 	void Update () {
 	
 	}
 
-    void OnCollisionEnter(Collision c)
-    {
-        if(c.transform.tag == "PlayerProjectile")
-        {
-            health -= c.gameObject.GetComponent<Projectile>().damage;
-            if(health == 0)
-            {
+    void OnTriggerEnter2D(Collider2D other) {
+        if (other.transform.tag == "PlayerProjectile") {
+            health -= other.gameObject.GetComponent<Projectile>().damage;
+            if (health <= 0) {
+                Instantiate(explosion, transform.position, Quaternion.identity);
                 Destroy(gameObject);
             }
+            Destroy(other.gameObject);
         }
     }
 }
--- Assets/Scripts/PathFollow.cs
+++ Assets/Scripts/PathFollow.cs
@@ -4,11 +4,18 @@ using System.Collections;
 public class PathFollow : MonoBehaviour {
 
     public Path path;
+    public bool faceForward;
     int currentPoint = 0;
     public float closeEnough = 0.1f;
     public float speed = 2.0f;
     [Range(0.0f, 1.0f)]
     public float inertia = 0.2f;
+
+    Rigidbody2D body;
+
+    void Start() {
+        body = GetComponent<Rigidbody2D>();
+    }
 
     // Update is called once per frame
     void Update () {
@@ -17,7 +24,8 @@ public class PathFollow : MonoBehaviour {
                 currentPoint += 1;
             } else {
                 //transform.position = Vector3.MoveTowards(transform.position, path.points[currentPoint].position, speed);
-                GetComponent<Rigidbody>().velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, (path.points[currentPoint].position - transform.position).normalized * speed, inertia);
+                body.velocity = Vector3.Lerp(body.velocity, (path.points[currentPoint].position - transform.position).normalized * speed, inertia);
+                transform.up = body.velocity.normalized;
             }
         } else {
             if (path.loops) currentPoint = 0;
