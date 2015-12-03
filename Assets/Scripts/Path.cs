0d47f225473765936d6a231f2cb7541d763ddc50
malaue4 <malaue4@gmail.com>
2015-12-03 10:47:24 +0100

    Fintud

---
+++ Assets/Scripts/Path.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Path : MonoBehaviour {
+
+    public bool loops = false;
+    public Transform[] points;
+
+    public void OnDrawGizmos() {
+        if (points.Length > 0) {
+            for (int i = 1; i < points.Length; i++) {
+                Gizmos.DrawLine(points[i - 1].position, points[i].position);
+            }
+            if (loops) {
+                Gizmos.DrawLine(points[points.Length - 1].position, points[0].position);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PathFollow.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PathFollow : MonoBehaviour {
+
+    public Path path;
+    int currentPoint = 0;
+    public float closeEnough = 0.1f;
+    public float speed = 2.0f;
+    [Range(0.0f, 1.0f)]
+    public float inertia = 0.2f;
+
+    // Update is called once per frame
+    void Update () {
+	    if(currentPoint < path.points.Length) {
+            if(Vector3.Distance(transform.position, path.points[currentPoint].position) < closeEnough) {
+                currentPoint += 1;
+            } else {
+                //transform.position = Vector3.MoveTowards(transform.position, path.points[currentPoint].position, speed);
+                GetComponent<Rigidbody>().velocity = Vector3.Lerp(GetComponent<Rigidbody>().velocity, (path.points[currentPoint].position - transform.position).normalized * speed, inertia);
+            }
+        } else {
+            if (path.loops) currentPoint = 0;
+            else Destroy(gameObject);
+        }
+	}
+}
