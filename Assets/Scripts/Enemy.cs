3fe7973516a9f7b58aa5e667a5751ae34a26bf2a
MadsChris92 <madschris888@gmail.com>
2015-12-03 12:00:02 +0100

    gg

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy : MonoBehaviour {
+
+
+    int health;
+	public enum enemyTypes {
+        Weak, Middle, Strong,
+    }
+    public enemyTypes type;
+
+	void Start () {
+        if (type == enemyTypes.Weak)
+        {
+            health = 2;
+        }
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision c)
+    {
+        if(c.transform.tag == "PlayerProjectile")
+        {
+            health--;
+            if(health == 0)
+            {
+                Destroy(gameObject);
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Spawner : MonoBehaviour {
+
+    public Path[] paths;
+    public GameObject[] enemies;
+
+    float time = 0, count = 0;
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        time += Time.deltaTime;
+        count += Time.deltaTime;
+        Debug.Log(time);
+        if (time < 5 && count > 0.5)
+        {
+            spawnEnemy(0, 0);
+            count = 0;
+        }
+	}
+
+    void spawnEnemy(int enem, int pat)
+    {
+        GameObject clone = Instantiate(enemies[enem], paths[pat].transform.position, Quaternion.identity) as GameObject;
+        clone.GetComponent<PathFollow>().path = paths[pat];
+    }
+}
