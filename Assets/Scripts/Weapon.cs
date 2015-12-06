9c5db2c2297bd599330de46d0f5599abe772b99e
MadsChris92 <madschris888@gmail.com>
2015-12-06 19:15:42 +0100

    d

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -3,7 +3,8 @@ using System.Collections;
 
 public class Weapon : MonoBehaviour {
 
-    public GameObject projectile;
+    public GameObject projectile, barrel;
+    GameObject player;
     public float shotsPerSecond;
     public Transform[] bulletSpawns;
     AudioSource audioSource;
@@ -12,7 +13,8 @@ public class Weapon : MonoBehaviour {
     public FireType fireType = FireType.All;
     public enum FireType {
         Alternate,
-        All
+        All,
+        Barrel
     }
     int fireAlt = 0;
 
@@ -35,12 +37,20 @@ public class Weapon : MonoBehaviour {
                 Instantiate(projectile, bulletSpawns[fireAlt].position, bulletSpawns[fireAlt].rotation);
                 fireAlt = (fireAlt + 1) % bulletSpawns.Length;
             }
+            else if(fireType == FireType.Barrel)
+            {
+                barrel.transform.rotation(player.transform.position);
+//                barrel.transform.LookAt(player.transform.position);
+                GameObject clone = Instantiate(projectile, bulletSpawns[0].position, bulletSpawns[0].rotation) as GameObject;
+                clone.GetComponent<Rigidbody2D>().AddForce(transform.forward * 1000);
+            }
 
         }
     }
 	
 	// Update is called once per frame
 	void Update () {
+        player = GameObject.FindGameObjectWithTag("Player");
         fireDelay = Mathf.Max(0, fireDelay-Time.deltaTime);
 	}
 }
