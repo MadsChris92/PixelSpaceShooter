7e456234b4b0f6078098809ed3232836a377a11b
MadsChris92 <madschris888@gmail.com>
2015-12-07 11:20:17 +0100

    k

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour {
     public Transform[] bulletSpawns;
     AudioSource audioSource;
     float fireDelay = 0;
+    Vector3 _dir;
 
     public FireType fireType = FireType.All;
     public enum FireType {
@@ -39,8 +40,9 @@ public class Weapon : MonoBehaviour {
             }
             else if(fireType == FireType.Barrel)
             {
-//               barrel.transform.rotation(player.transform.position);
-//                barrel.transform.LookAt(player.transform.position);
+             
+                //               barrel.transform.rotation(player.transform.position);
+                //                barrel.transform.LookAt(player.transform.position);
                 GameObject clone = Instantiate(projectile, bulletSpawns[0].position, bulletSpawns[0].rotation) as GameObject;
                 clone.GetComponent<Rigidbody2D>().AddForce(transform.forward * 1000);
             }
@@ -50,7 +52,16 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        player = GameObject.FindGameObjectWithTag("Player");
+        if(fireType == FireType.Barrel)
+        {
+            player = GameObject.FindGameObjectWithTag("Player");
+            Vector3 targetDir = player.transform.position - barrel.transform.position;
+            Vector3 newDir = Vector3.RotateTowards(barrel.transform.position, targetDir, 5, 0.0F);
+            Debug.DrawRay(transform.position, newDir, Color.red);
+            barrel.transform.rotation = Quaternion.LookRotation(newDir);
+
+        }
+        
         fireDelay = Mathf.Max(0, fireDelay-Time.deltaTime);
 	}
 }
